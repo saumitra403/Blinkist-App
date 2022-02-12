4b854cf8b21be1d10d63da8ea18123c9abd93ca0
Saumitra Mishra <47312819+saumitra403@users.noreply.github.com>
2022-02-12 22:25:26 +0530

    Delete src/components/atoms/logo directory

--- src/components/atoms/logo/index.stories.tsx
+++
@@ -1,8 +0,0 @@
-import Logo from './index';
-import { ComponentStory } from '@storybook/react';
-export default {
-    title: 'atoms/Logo',
-    component: Logo
-};
-
-export const Template: ComponentStory<typeof Logo> = (args) => <Logo {...args}/>;
--- src/components/atoms/logo/index.tsx
+++
@@ -1,9 +0,0 @@
-type LogoProps = {
-    className? : string
-}
-
-const Logo = (props: LogoProps) => {
-    return (<img src="/Assets/Blinklist 1.jpg" className={props.className} data-testid="box"></img>)
-}
-
-export default Logo;
