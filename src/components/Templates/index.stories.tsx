7de918a4778131b8162156522d41b3104060337e
Saumitra <saumitra.mishra@zemosolabs.com>
2022-02-21 14:12:17 +0530

    Fixing Folder Structure

--- src/components/Templates/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Template from "."
-import { ComponentStory } from "@storybook/react";
-export default {
-    title: 'Templates',
-    component: Template
-}
-
-
-export const Templates: ComponentStory<typeof Template> = () => <Template />;
