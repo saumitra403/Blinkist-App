75eaf174ff75314694785695b951618463ead996
Saumitra <saumitra.mishra@zemosolabs.com>
2022-03-30 23:24:43 +0530

    Added Global Theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Template from "./components/Templates";
 import baseTheme from "./Theme/mainTheme";
 import {ThemeProvider} from '@mui/material';
 function App() {
-
     return <ThemeProvider theme={baseTheme}><Template data-testid="box"/></ThemeProvider>
 }
 export default App;
